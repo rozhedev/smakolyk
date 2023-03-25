aa8862f5572349842313b5972d2ae343013cd638
rozhedev <digital1479@gmail.com>
2023-03-25 11:28:15 +0200

    Create promo section

--- #src/js/libs/swiper.js
+++ #src/js/libs/swiper.js
@@ -1,10 +1,18 @@
-// import Swiper from 'swiper/bundle';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-// });
+const promoSlider = new Swiper('.promo-slider', {
+    // Optional parameters
+    modules: [Navigation, Pagination],
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
