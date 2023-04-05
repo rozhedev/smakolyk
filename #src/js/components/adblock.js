e16d0101694a66b86c1707a17d7925305cdbce8b
rozhedev <digital1479@gmail.com>
2023-04-05 08:45:10 +0300

    Create adblock detector

--- #src/js/components/adblock.js
+++ #src/js/components/adblock.js
@@ -1,21 +1,34 @@
 import { STATE_LIST } from '../values';
 
-// const detect = document.querySelector("#detect");
-// const adblockpopup = document.querySelector(".adblock-popup");
-// const button = adblockpopup.querySelector(".adblock-popup__btn");
+const detect = document.querySelector("#detect");
+const advertPopup = document.querySelector(".advert-popup");
+const button = advertPopup.querySelector(".advert-popup__btn");
+let showInterval = 10000;
 
-// let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
-// for (let item of adClasses) {
-//     detect.classList.add(item);
-// }
+let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
+for (let item of adClasses) {
+    detect.classList.add(item);
+}
 
-// let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
-// if (!adblockpopup.classList.contains(STATE_LIST.show)) {
-//     getProperty == "none" ? adblockpopup.classList.add(STATE_LIST.show) : adblockpopup.classList.remove(STATE_LIST.show);
-// }
+let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
 
-// if(button) {
-//     button.addEventListener("click", () => {
-//         adblockpopup.classList.remove(STATE_LIST.show);
-//     });
-// }
+setInterval(() => {
+    if (!advertPopup.classList.contains(STATE_LIST.show)) {
+        if (getProperty == "none") {
+            advertPopup.classList.add(STATE_LIST.show);
+            document.body.classList.add(STATE_LIST.lock);
+        } else { 
+            advertPopup.classList.remove(STATE_LIST.show); 
+            document.body.classList.remove(STATE_LIST.lock);
+        }
+    }
+}, showInterval)
+
+if (button) {
+    button.addEventListener("click", () => {
+        advertPopup.classList.remove(STATE_LIST.show);
+        document.body.classList.remove(STATE_LIST.lock);
+        // * Work incorrect
+        window.location.reload();
+    });
+}
